667c3863c8fb938f2a619786e8c9557c52582d11
Gnoth-404 <tienanh06122001@gmail.com>
2022-05-11 13:58:07 +0100

    Backup triggered on 2022-05-11 13:58:07
    Klipper on commit: c263f76
    
    Moonraker on commit: 56097a3

--- scripts/auto_backup.sh
+++ scripts/auto_backup.sh
@@ -57,7 +57,7 @@ grab_version(){
 push_config(){
   cd $config_folder
   echo Pushing updates
-  git pull -v main
+  git pull -v 
   git add . -v
   current_date=$(date +"%Y-%m-%d %T")
   git commit -m "Backup triggered on $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
